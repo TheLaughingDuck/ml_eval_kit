244fb90e7bbc3641e74c12a87e4eb95e6c1740c2
Simon Jorstedt <jorstedtsimon@gmail.com>
2025-05-27 13:46:42 +0200

    Simplified confusion matrix creation, and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ml_eval_kit
-A python package
+A python package for evaluation of trained machine learning models.
+
+
+
+# Example usage
+
+## Plots
+The functions `get_conf_matrix` and `create_conf_matrix_fig` can be used to create a confusion matrix figure, given some true labels and predictions.
+```
+from ml_eval.plots import get_conf_matrix, create_conf_matrix_fig
+
+m = get_conf_matrix([0, 1, 2, 0, 1, 2],
+                    [0, 1, 2, 1, 0, 2])
+
+create_conf_matrix_fig(m, classes=["A", "B", "C", "D", "E"],
+                       title="Test Confusion Matrix",
+                       subtitle="This is a test subtitle")
+```
+
+![alt text](images/confusion_matrix.png)
--- ml_eval/plots.py
+++ ml_eval/plots.py
@@ -1,15 +1,29 @@
 
-
-def get_conf_matrix(all_preds, all_targets, n_classes=3) -> list:
+#%%
+def get_conf_matrix(all_preds, all_targets, n_classes=None, verbose=0) -> list:
     '''
     Takes two integer lists of all target classes, and all predictions by some classifier.
 
     Returns a confusion matrix, with true class on rows, and predicted class on the columns,
     as per https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+
+    Args:
+        all_preds (list): List of predicted classes, as consecutive integers, i.e. 0,1,2,3,..., C.
+        all_targets (list): List of true classes, as consecutive integers, i.e. 0,1,2,3,..., C.
+        n_classes (int): Number of classes. If None, it is inferred from the data. Specify if some classes might be missing in the predictions.
+
+        verbose (int): Verbosity level. If 0, no output is printed. If 1, prints the number of predictions in all_preds.
     '''
+    # Infer number of classes from the data if not specified
+    if n_classes is None: n_classes = max(max(all_preds), max(all_targets)) + 1
+
+    # Setup empty confusion matrix
     matrix = [[0 for i in range(n_classes)] for i in range(n_classes)]
 
-    print(f"There are {len(all_preds)} predictions in all_preds.")
+    if verbose > 0:
+        print(f"Number of classes: {n_classes}")
+        print(f"There are {len(all_targets)} targets in all_targets.")
+        print(f"There are {len(all_preds)} predictions in all_preds.")
 
     for i in range(n_classes):
         for j in range(n_classes):
@@ -24,37 +38,29 @@ def get_conf_matrix(all_preds, all_targets, n_classes=3) -> list:
 import matplotlib.pyplot as plt
 import numpy as np
 
-def create_conf_matrix_fig(conf_matrix, save_fig_as=None, epoch=None, title="", n_classes=3, subtitle=None):
+def create_conf_matrix_fig(conf_matrix, classes, save_fig_as=None, title="", subtitle=None):
     '''
     Takes confusion matrices (on training and validation data),
     and creates a figure with them. Saves as a png.
 
     The true classes are on the rows, and the predicted values on the columns.
+
+    Args:
+        conf_matrix (list): Confusion matrix, as returned by get_conf_matrix().
+        save_fig_as (str): Path to save the figure as a png file.
+        title (str): Title of the figure.
+        subtitle (str): Subtitle of the figure.
     '''
     fig, axs = plt.subplots(ncols=1)
-    #fig.tight_layout(rect=(0,0,1,0.999))
 
-    # axs[0].matshow(train_mat)
-    # for (i, j), z in np.ndenumerate(train_mat):
-    #     axs[0].text(j, i, '{}'.format(z), ha='center', va='center')
-    # axs[0].set_yticks(ticks=[0,1,2], labels=["Gli", "Epe", "Med"])
-    # axs[0].set_xticks(ticks=[0,1,2], labels=["Gli", "Epe", "Med"])
-    # axs[0].set_xlabel("True value")
-    # axs[0].set_ylabel("Prediction")
-    # axs[0].set_title("Training data")
-
-    if n_classes == 3:
-        labels = ["Gli", "Epe", "Med"]
-    elif n_classes == 2:
-        labels = ["Supra", "Infra"]
-    else:
-        raise ValueError("Unsupported number of classes")
+    n_classes = len(conf_matrix)
+    if classes is None: classes = [str(i) for i in range(n_classes)]
 
     axs.matshow(conf_matrix, alpha=0.3, cmap="Blues")
     for (i, j), z in np.ndenumerate(conf_matrix):
         axs.text(j, i, '{}'.format(z), ha='center', va='center', size="xx-large")
-    axs.set_yticks(ticks=[i for i in range(n_classes)], labels=labels, fontsize=12)
-    axs.set_xticks(ticks=[i for i in range(n_classes)], labels=labels, fontsize=12)
+    axs.set_yticks(ticks=[i for i in range(n_classes)], labels=classes, fontsize=12)
+    axs.set_xticks(ticks=[i for i in range(n_classes)], labels=classes, fontsize=12)
     axs.set_ylabel("True value", fontsize=16)
     axs.set_xlabel("Prediction", fontsize=16)
     #axs.set_title("Validation data")
@@ -62,8 +68,6 @@ def create_conf_matrix_fig(conf_matrix, save_fig_as=None, epoch=None, title="", 
     if subtitle != None: plt.title(subtitle)
 
     if save_fig_as != None:
-        #start_date = re.search(r"\d{4}-\d{2}-\d{2}-\d{2}:\d{2}:\d{2}", save_fig_as).group(0)
-        #fig.suptitle(title+" (Epoch "+str(epoch)+")", fontsize=16)
         fig.suptitle(title, fontsize=20)
         fig.tight_layout()
         fig.savefig(save_fig_as)
@@ -72,3 +76,9 @@ def create_conf_matrix_fig(conf_matrix, save_fig_as=None, epoch=None, title="", 
         fig.suptitle(title, fontsize=16)
         fig.tight_layout()
         fig.show()
+
+
+m = get_conf_matrix([0, 1, 2, 0, 1, 2], [0, 1, 2, 1, 0, 2], n_classes=5)
+create_conf_matrix_fig(m, save_fig_as=None, classes=["A", "B", "C", "D", "E"], title="Test Confusion Matrix", subtitle="This is a test subtitle")
+
+# %%
